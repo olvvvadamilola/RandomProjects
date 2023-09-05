3d09776e911d09153b8da3082b3ab4931d8850ad
olvwadamilola <destric3@gmail.com>
2023-09-05 21:02:33 +0000

    changes

--- Execute_and_Push/epx.sh
+++ Execute_and_Push/epx.sh
@@ -3,12 +3,6 @@
 #Get name of file to be executed
 file_name=$1
 
-#check if file name exists
-if [ -z "$1" ]; then
-echo "please provide the file name"
-exit 1
-fi
-
 #check if file exists
 if [ -f "$file_name" ]; then
 # Make file executable
