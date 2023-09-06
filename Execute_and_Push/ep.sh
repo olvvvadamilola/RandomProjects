34c9b0d5c3ffb0112d307caa6e63d707be46aa37
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-09-06 00:52:16 +0000

    Added functions to make the script clearer

--- Execute_and_Push/ep.sh
+++ Execute_and_Push/ep.sh
@@ -1,37 +1,62 @@
 #!/bin/bash
 
-#Get name of file to be executed
-file_name=$1
+# Get name of file to be executed
+filename=$1
 
-#check if file exists
-if [ -f "$file_name" ]; then
-# Make file executable
-chmod +x  "$file_name"
+function show_usage() {
+	echo "Usage: ep <filename>"
+	exit 1
+}
 
-#prompt user for a commit message
-read -p "Enter commit message or press ENTER to use '$file_name' by default:" commit_message
-if [ -z "$commit_message" ]; then 
-commit_message="$file_name"
-fi
+function get_file() {
+	# check if file exists
+	if [ -f "$filename" ]; then
+		# Make file executable and get commit message
+		chmod +x  "$filename"
+		get_commit_msg
+	elif [ -z "$filename" ]; then
+		show_usage
+	else # throw error and exit
+		echo "$filename: No such file"
+		show_usage
+	fi
+}
 
-#Add newly executable file to GIT
-git add "$file_name"
+function get_commit_msg() {
+	#prompt user for a commit message
+	echo "Enter a commit message or press ENTER to use '$filename' (not recommended)"
+	read commit_message
 
-#Use entered commit message
-git commit -m  "$commit_message"
+	if [ -z "$commit_message" ]; then 
+		echo "Defaulting to '$filename' as commit message. This is not recommended"
+		commit_message="$filename"
+	fi
+	perform_git_ops
+}
 
-#push changes to remote REPOSITORY
-git push
+function perform_git_ops {
+	#Add newly executable file to GIT
+	git add "$filename"
+	#Use entered commit message
+	git commit -m "$commit_message"
+	#push changes to remote REPOSITORY
+	git push
 
-#send user message confirming successful commit and push of file
-echo "changes successfully commited and pushed for $file_name, you can now use the checker."
-else
-echo "Error 404 $file_name not found"
-fi
+	final_checks
+}
 
+function final_checks() {
+	#send user message confirming successful commit and push of file
+	if [ $? -eq 0 ]; then
+		echo "changes successfully commited and pushed for $filename, you can now use the checker."
+		echo "Here your latest log $(git log --name-status --online | head -2)"
+	else
+		echo
+		echo "An error occurred while pushing your commit... Please check your connection"
+		echo "Once it's solved, manually do a git push"
+		exit 1
+	fi
+}
 
-
-
-
-#olvwadamilola
-#please view README.md
+# start
+get_file
